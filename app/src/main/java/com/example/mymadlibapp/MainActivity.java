49462d9776b99b48a590747b0ab53a21b3399816
apple <malinagt@outlook.com>
2023-05-25 23:23:31 -0700

    commit

--- app/src/main/java/com/example/mymadlibapp/MainActivity.java
+++ app/src/main/java/com/example/mymadlibapp/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.mymadlibapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -28,7 +30,7 @@ public class MainActivity extends AppCompatActivity {
 
     private EditText etColor;
 
-    private Button btnFriend;
+    private Button btnCompile;
 
 
 
@@ -36,6 +38,55 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etFriend = findViewById(R.id.et_friend);
+        etFriendPronoun = findViewById(R.id.et_friendpronoun);
+        etName = findViewById(R.id.et_name);
+        etYourPronoun = (R.id.et_yourpronoun);
+        etBirthdate = findViewById(R.id.et_birthdate);
+        etBirthYear = findViewById(R.id.et_birthyear);
+        etRandoNumber = findViewById(R.id.et_randonumber);
+        etBoolean = findViewById(R.id.et_boolean);
+        etBoolean2 = findViewById(R.id.et_boolean2);
+        etColor = findViewById(R.id.et_color);
+        btnCompile = findViewById(R.id.btn_compile);
+
+        Intent intentDisplay = new Intent(MainActivity.this, DisplayActivity.class);
+
+        btnCompile.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                String story = "";
+                story += "...";
+                story += "\nOn a "
+                story += "\n "
+
+                intentDisplay.putExtra("story", story);
+                startActivity(intentDisplay);
+
+
+
+
+
+
+
+            }
+        });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
 
--- app/src/main/java/com/example/mymadlibapp/SecondActivity.java
+++ app/src/main/java/com/example/mymadlibapp/SecondActivity.java
@@ -1,14 +1,29 @@
 package com.example.mymadlibapp;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
 
 public class SecondActivity extends AppCompatActivity {
+
+private TextView tvStory
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+            tvStory = findViewById(R.id.tv_story);
+
+            Intent intent = getIntent();
+
+            String story = intent.getStringExtra( "story");
+
+            tvStory.setText(story);
+
+
+
+
+
     }
 }
